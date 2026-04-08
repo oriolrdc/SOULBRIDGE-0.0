80eaba521822fa0bb349499b6cf7cce27c3ecf0a
oriolrdc <oroca@ctsformacion.com>
2026-04-08 23:04:18 +0200

    ??

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -5,6 +5,7 @@ public class RoomManager : MonoBehaviour
     [SerializeField] Transform[] spawnPoints;
     [SerializeField] float raound;
     [SerializeField] float maxRound;
+    [SerializeField] 
 
     void Start()
     {
